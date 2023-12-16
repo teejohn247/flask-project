7326665a9c626e230ef553d527643e02d4e46a65
aghosby <etiboaghogho@gmail.com>
2023-12-16 15:27:05 +0000

    Initial test

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, render_template
 
 # Create the Flask app instance
 app = Flask(__name__)
 
 # Define a route for the root URL
-@app.route('/')
-def home():
-    return "Hello, world!"
+@app.route('/', methods=['GET', 'POST'])
+def start():
+    return render_template('index.html')
 
 # Run the app if this script is executed directly
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
